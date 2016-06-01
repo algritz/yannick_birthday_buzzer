1f15709e066c5ed1f5579051e4baa28858ccb74d
David Cloutier <david.cloutier@oracle.com>
2016-06-01 09:26:15 -0400

    fixed date calculation

--- app/src/main/java/ca/nosrecettes/ybb/yannick_birthday_buzzer/MainActivity.java
+++ app/src/main/java/ca/nosrecettes/ybb/yannick_birthday_buzzer/MainActivity.java
@@ -1,13 +1,22 @@
 package ca.nosrecettes.ybb.yannick_birthday_buzzer;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,27 +28,83 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        long days_to_go = 0;
 
-        Calendar currentCalendar = Calendar.getInstance();
-
-        Date today = new Date();
-
-        long next_year = currentCalendar.get(Calendar.YEAR) + 1;
-        currentCalendar.set(Calendar.MONTH, 5);
-        currentCalendar.set(Calendar.DAY_OF_MONTH, 15);
-
-        Date next_bday = currentCalendar.getTime();
-
-        long diff = next_bday.getTime() - today.getTime();
-
-
-        //long diff_days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-        long a_year_millisec = TimeUnit.MILLISECONDS.convert(365, TimeUnit.DAYS);
-
-        days_to_go = 365 - (TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
+        Calendar calendarPresent = Calendar.getInstance();
+        Calendar calendarBirthday = Calendar.getInstance();
+        calendarPresent.getTime();
+        calendarBirthday.set(Calendar.YEAR, calendarPresent.get(Calendar.YEAR) +1);
+        calendarBirthday.set(Calendar.MONTH, 4);
+        calendarBirthday.set(Calendar.DAY_OF_MONTH, 15);
+        long milliseconds1 = calendarPresent.getTimeInMillis();
+        long milliseconds2 = calendarBirthday.getTimeInMillis();
+        long diff = milliseconds2 - milliseconds1;
+        long diffDays = TimeUnit.MILLISECONDS.toDays(diff);
 
         TextView days_to_go_text = (TextView) findViewById(R.id.days_to_go);
-        days_to_go_text.setText(days_to_go + " days to go!");
+        days_to_go_text.setText(diffDays + " days to go!");
+        sendSMS("14185615589", diffDays + " days to go!");
     }
+
+
+    private void sendSMS(String phoneNumber, String message) {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+    }
+
+
 }
